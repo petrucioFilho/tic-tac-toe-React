3cba5abddda8a6f3e5a8a2b18e549ccc39b1a37f
petrucio.flimed <petrucio.flimed@gmail.com>
2025-01-19 01:03:23 -0300

    Add function restart and add status

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@mui/material";
 import Square from "./Square";
 import { useState } from "react";
+import { calculateWinner } from "../function/function";
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i: number) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -18,9 +27,16 @@ function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
+  function restart() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
-      <div className="status">Next player: X</div>
+      <div className="status">{status}</div>
       <div className="board">
         <div className="board-row">
           <Square value={squares[0]} onHandleClick={() => handleClick(0)} />
@@ -39,7 +55,12 @@ function Board() {
         </div>
       </div>
 
-      <Button variant="contained" style={{ marginTop: "15px" }} size="small">
+      <Button
+        variant="contained"
+        style={{ marginTop: "15px" }}
+        size="small"
+        onClick={() => restart()}
+      >
         restart
       </Button>
     </div>
